fe29074d237e5705d92b39fee51734f4875f5ebc
fabioferrari00 <fabioferrari.ff00@gmail.com>
2025-07-09 15:54:25 +0200

    [fix] condizione per farla accendere, da fixare lo spegnimento

--- js/script.js
+++ js/script.js
@@ -4,7 +4,11 @@ const button = document.getElementById('my-button');
 const image = document.getElementById('pic');
 
 button.addEventListener('click', function () {
-
-  image.src = './img/yellow_lamp.png';
-
-})
+  if (image.src = './img/white_lamp.png') {
+    image.src = './img/yellow_lamp.png';
+    button.innerText = 'Spegni';
+  } else {
+    image.src = './img/white_lamp.png';
+    button.innerText = 'Accendi';
+  }
+});
