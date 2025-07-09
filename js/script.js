b8cf6f24655723ef6eb95f1bd11e583c73bd188a
fabioferrari00 <fabioferrari.ff00@gmail.com>
2025-07-09 13:17:48 +0200

    [fix] add js for lamp on

---
+++ js/script.js
@@ -0,0 +1,10 @@
+//dichiarazione delle variabili
+
+const button = document.getElementById('my-button');
+const image = document.getElementById('pic');
+
+button.addEventListener('click', function () {
+
+  image.src = './img/yellow_lamp.png';
+
+})
